e7c61a62081e67e3bd83db802063c22e025c003b
riccardo1973 <55211945+riccardo1973@users.noreply.github.com>
2025-11-08 21:57:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"status": "Calybra ML Engine running"}
+
+@app.post("/predict")
+def predict():
+    return {"prediction": "demo_value"}
